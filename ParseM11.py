4f941966e3d18721168b96c59292616e38ab89be
Kirill Sukhorukov <123446875+K1rsN7@users.noreply.github.com>
2023-10-15 06:22:45 +0300

    Added files

--- ParseM11.py
+++ ParseM11.py
@@ -1,41 +1,44 @@
 import json
 from PyPDF2 import PdfFileReader as pd
 import os
+import re
+import time
+
 json_programm = {}
-#Открываем файл конструктором With
+# Открываем файл конструктором With
 for filename in os.listdir("dataset/М-11/"):
-    with open("dataset/М-11/"+filename, 'rb') as pdf_file:
-        pdf_open_reader = pd(pdf_file) #Открываем PDF на чтение
-        #Циклом обходим все страницы книги и выводим их содержание на печать
+    errors = []
+    with open("dataset/М-11/" + filename, 'rb') as pdf_file:
+        pdf_open_reader = pd(pdf_file)  # Открываем PDF на чтение
+        # Циклом обходим все страницы книги и выводим их содержание на печать
         parse_text = ""
         for page_num in range(pdf_open_reader.numPages):
             pdf_page = pdf_open_reader.getPage(page_num)
-            # print(f"Page:  {page_num +1}")
             text = pdf_page.extractText("")
-            # Логика прыжком по значениям
+            # Логика прыжком по парсенному тексту
             list = {
                 "ТРЕБОВАНИЕ-НАКЛАДНАЯ №  ": "\n",
                 "Организация ": "по ОКПО",
-                "по ОКПО ":"\n",
+                "по ОКПО ": "\n",
                 "Структурное\nподразделение": "Отправитель",
-                "(работ,\nуслуг)": "Через кого", # Таблица
+                "(работ,\nуслуг)": "Через кого",  # Таблица
                 "Через кого": "Затребовал",
                 "Затребовал": "Разрешил",
                 "Разрешил": "Материальные",
                 "1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16": "Отпустил",
-                "Отпустил" : "(должность)",
-                "(должность)":"(подпись)",
-                "(подпись)":"(расшифровка подписи)Получил",
-                "расшифровка подписи)Получил":"(должность)",
-                "должность)":"(подпись)",
-                "подпись)":"(расшифровка подписи)",
-                "расшифровка подписи)":"Документ подписан электронной подписью",
+                "Отпустил": "(должность)",
+                "(должность)": "(подпись)",
+                "(подпись)": "(расшифровка подписи)Получил",
+                "расшифровка подписи)Получил": "(должность)",
+                "должность)": "(подпись)",
+                "подпись)": "(расшифровка подписи)",
+                "расшифровка подписи)": "Документ подписан электронной подписью",
             }
             for str_start, str_end in list.items():
                 index_start = text.find(str_start) + len(str_start)
                 index_end = text[index_start:].find(str_end)
-                parse_text += text[index_start: index_start+ index_end] + " \\/"
-                text=text[index_start+index_end:]
+                parse_text += text[index_start: index_start + index_end] + " \\/"
+                text = text[index_start + index_end:]
 
     keys = [
         "ТРЕБОВАНИЕ-НАКЛАДНАЯ №", "Организация", "по ОКПО", "Структурное подразделение",
@@ -44,14 +47,113 @@ for filename in os.listdir("dataset/М-11/"):
     ]
     json_for = {}
     elements = parse_text.replace("\n", " ").split("\\/")
+    # Обработка ошибок
     for index in range(0, len(keys)):
-        if keys[index] == "Структурное подразделение":
-            json_for[keys[index]] = " ".join(elements[index].split())[:-4]
-            json_for["БЕ"] = " ".join(elements[index].split())[-4:]
+        key = keys[index]
+        if key == "Структурное подразделение":
+            value = " ".join(elements[index].split())[:-4]
+            be = " ".join(elements[index].split())[-4:]
+            if value:
+                json_for[key] = value
+            else:
+                errors.append(f"Отсутствует значение для ключа '{key}'")
+            if be:
+                json_for["БЕ"] = be
+            else:
+                errors.append(f"Отсутствует значение для ключа 'БЕ'")
+        elif key == "по ОКПО":
+            len_okpo = len(" ".join(elements[index].split()))
+            if len_okpo < 8 or len_okpo > 10:
+                errors.append(f'Некорректный ОКПО {key}')
+            json_for[key] = " ".join(elements[index].split())
+        elif key == "Первая таблица":
+            table = elements[index][1:]
+            date_table = table.split(" ")[0]
+            try:
+                time.strptime(date_table, '%d/%m/%Y')
+            except ValueError:
+                errors.append("Некорректно заполнена дата составления в таблице")
+            table = " ".join(table.split(" ")[1:])
+            table = table[3:]
+            nomnumber_table = table[:table[1:].find(f"{table[0:3]}")]
+            if nomnumber_table[4] != " ":
+                errors.append("Некорректно заполнен 'отправитель,структурное подразделений'")
+            table = table[table[1:].find(f"{table[0:3]}") + 1:]
+            re_itog = re.search(r'[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]', table)
+            if re_itog:
+                strpodrazdel = table[:re_itog.span()[0] - 3]
+                subschet = table[re_itog.span()[0]:re_itog.span()[1]]
+            else:
+                strpodrazdel = table[:table.find("- - ")]
+                subschet = None
+            if not subschet:
+                errors.append("Некорректно заполнен субсчёт в таблице")
+            if strpodrazdel[4] != " ":
+                errors.append("Некорректно заполнен 'получатель, структурное подразделение'")
+        elif key == "Вторая таблица":
+            table = elements[index][1:-3]
+            re_itog = re.search(r'[0-9][0-9][0-9][0-9] [0-9][0-9][0-9][0-9][0-9][0-9]', table)
+            if re_itog:
+                subschet = "".join(table[re_itog.span()[0]:re_itog.span()[1]].split())
+                table = table[re_itog.span()[1]:]
+            else:
+                subschet = None
+                table = table[1:]
+            if not subschet:
+                errors.append("Некорректно заполнено поле 'Корреспондирующий счет, субсчет' в таблице")
+            re_itog = re.search(r'[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9] [0-9][0-9]', table)
+            if re_itog:
+                nomnumber_table2 = "".join(table[re_itog.span()[0]:re_itog.span()[1]].split())
+                naimen_table = table[:re_itog.span()[0]]
+                table = table[re_itog.span()[1]:]
+            else:
+                nomnumber_table2 = table[:table.find("- ")]
+                naimen_table = None
+            if not naimen_table:
+                errors.append("Некорректно заполнено поле 'Материальные ценности, номенклатурный номер'")
+            re_itog = re.search(r' [0-9][0-9][0-9] ', table)
+            if re_itog:
+                code_unit = table[re_itog.span()[0] + 1:re_itog.span()[1] - 1]
+                table = table[re_itog.span()[1]:]
+            else:
+                code_unit = None
+            re_itog = re.search(r'[0-9]', table)
+            if re_itog:
+                ed_iz = table[:re_itog.span()[0]]
+                table = table[re_itog.span()[0]:]
+            else:
+                ed_iz = None
+            number_zadreb = table[:table.find(".") + 4]
+            table = table[table.find(".") + 5:]
+            number_otpush = table[:table.find(".") + 4]
+            table = table[table.find(".") + 5:]
+            price = table[:table.find(",") + 3]
+            table = table[table.find(",") + 4:]
+            sum = table[:table.find(",") + 3]
+            table = table[table.find(",") + 4:]
+            try:
+                if ("".join(number_otpush.split(" "))) not in "".join(number_zadreb.split(" ")):
+                    errors.append("Количество затраченного и отпущенного не сходится в таблице")
+            except:
+                errors.append("Некорректно указано количество затраченного и отпущенного не сходится в таблице")
         else:
-            json_for[keys[index]] = " ".join(elements[index].split())
-    json_programm[filename] = json_for
-    print(json_for)
+            value = " ".join(elements[index].split())
+            if value:
+                json_for[key] = value
+            else:
+                errors.append(f"Отсутствует значение для '{key}'")
+
+    print("Название файла: " + filename)
+    errors_json = []
+    if len(errors) == 0:
+        errors = "Корректно заполнен"
+    else:
+        print("Список ошибок: ")
+        for error in set(errors):
+            errors_json += error
+            print("     " + error)
+    json_programm[filename] = errors_json
+    print("-" * 30)
 
 M_11_json = json.dumps(json_programm)
 with open("M-11.json", "w") as my_file:
--- ParserФМУ76.py
+++ ParserФМУ76.py
@@ -1,13 +1,16 @@
 import json
-from pprint import pprint
+import re
+import time
+
 from PyPDF2 import PdfFileReader as pd
 import os
 
 json_programm = {}
 # Открываем файл конструктором With
 for filename in os.listdir("dataset/ФМУ-76/"):
+    errors = []
     try:
-        with open("dataset/ФМУ-76/"+filename, 'rb') as pdf_file:
+        with open("dataset/ФМУ-76/" + filename, 'rb') as pdf_file:
             pdf_open_reader = pd(pdf_file)  # Открываем PDF на чтение
             # Циклом обходим все страницы книги и выводим их содержание на печать
             parse_text = ""
@@ -49,7 +52,7 @@ for filename in os.listdir("dataset/ФМУ-76/"):
                         text = text[index_start + index_end:]
                     keys = [
                         "Форма по ОКУД", "по ОКПО", "Структурное подразделение", "БЕ",
-                        "АКТ_таблица", "УТВЕНикелинАЮ_должность", "УТВЕНикелинАЮ_хз", "УТВЕНикелинАЮ_дата",
+                        "АКТ_таблица", "УТВЕНикелинАЮ_должность", "УТВЕНикелинАЮ", "УТВЕНикелинАЮ_дата",
                         "Первая таблица", "Материально ответственное лицо", "Направление расхода", "Инвентарный номер",
                         "Коммисия в составе", "Вторая таблица", "Председатель комисии", "Члены коммисии",
                         "Мат_отв_лицо_должность", "Мат_отв_лицо_подпись", "Мат_отв_лицо_ФИО", "Дата"
@@ -81,7 +84,8 @@ for filename in os.listdir("dataset/ФМУ-76/"):
                                 json_for[f"Член_коммисии{id_komis}_должность"] = " ".join(
                                     stroka[:stroka.find("(должность)")].split())
                                 stroka = stroka[stroka.find("(должность)") + len("(должность)"):]
-                                json_for[f"Член_коммисии{id_komis}_подпись"] = " ".join(stroka[:stroka.find("(подпись)")].split())
+                                json_for[f"Член_коммисии{id_komis}_подпись"] = " ".join(
+                                    stroka[:stroka.find("(подпись)")].split())
                                 stroka = stroka[stroka.find("(подпись)") + len("(подпись)"):]
                                 json_for[f"Член_коммисии{id_komis}_рашифровка_подписи"] = " ".join(
                                     stroka[:stroka.find("(расшифровка подписи)")].split())
@@ -108,13 +112,13 @@ for filename in os.listdir("dataset/ФМУ-76/"):
                         index_end = text[index_start:].find(str_end)
                         parse_text += text[index_start: index_start + index_end] + "\\/"
                         text = text[index_start + index_end:]
-                    keys = [
+                    keys2 = [
                         "Вторая таблица", "Председатель комисии", "Члены коммисии",
                         "Мат_отв_лицо_должность", "Мат_отв_лицо_подпись", "Мат_отв_лицо_ФИО", "Дата"
                     ]
                     elements = parse_text.replace("\n", " ").split("\\/")
-                    for index in range(0, len(keys)):
-                        if (keys[index] == "Председатель комисии"):
+                    for index in range(0, len(keys2)):
+                        if (keys2[index] == "Председатель комисии"):
                             stroka = " ".join(elements[index].split())
                             for i in range(stroka.count("(должность)")):
                                 json_for[f"Председатель{id_predsed}_должность"] = " ".join(
@@ -127,7 +131,7 @@ for filename in os.listdir("dataset/ФМУ-76/"):
                                     stroka[:stroka.find("(расшифровка подписи)")].split())
                                 stroka = stroka[stroka.find("(расшифровка подписи)") + len("(расшифровка подписи)"):]
                                 id_predsed += 1
-                        elif keys[index] == "Члены коммисии":
+                        elif keys2[index] == "Члены коммисии":
                             stroka = " ".join(elements[index].split())
                             for i in range(stroka.count("(должность)")):
                                 json_for[f"Член_коммисии{id_komis}_должность"] = " ".join(
@@ -141,18 +145,141 @@ for filename in os.listdir("dataset/ФМУ-76/"):
                                 stroka = stroka[stroka.find("(расшифровка подписи)") + len("(расшифровка подписи)"):]
                                 id_komis += 1
                         else:
-                            if keys[index] not in json_for.keys():
-                                json_for[keys[index]] = " ".join(elements[index].split())
+                            if keys2[index] not in json_for.keys():
+                                json_for[keys2[index]] = " ".join(elements[index].split())
                             else:
-                                json_for[keys[index]] += " ".join(elements[index].split())
+                                json_for[keys2[index]] += " ".join(elements[index].split())
             json_for["Дата"] += " г."
-        print(json_for)
-        json_programm[filename] = json_for
     except:
-        print("Произошла ошибка!")
+        pass
+
+    print("Название файла: " + filename)
+    # print(json_for)
+    keys = [
+        "Форма по ОКУД", "Организация", "по ОКПО", "Структурное подразделение", "БЕ",
+        "АКТ_номер", "АКТ_дата", "УТВЕНикелинАЮ_должность", "УТВЕНикелинАЮ_дата",
+        "Первая таблица", "Материально ответственное лицо", "Направление расхода", "Инвентарный номер",
+        "Коммисия в составе", "Вторая таблица",
+        # "Председатель комисии", "Члены коммисии",
+        "Мат_отв_лицо_должность", "Мат_отв_лицо_подпись", "Мат_отв_лицо_ФИО", "Дата"
+    ]
+    # Обработка ошибок
+    for index in range(0, len(keys)):
+        if json_for != {}:
+            key = keys[index]
+            if key == "по ОКПО":
+                if len(json_for[key]) != 8:
+                    errors.append(f"Некорректный код по ОКПО")
+            elif key == "БЕ":
+                if len(json_for[key]) != 4:
+                    errors.append(f"Некорректный код БЕ")
+            elif key == "АКТ_дата":
+                date = json_for[key]
+                try:
+                    time.strptime(date, '%d/%m/%Y')
+                except ValueError:
+                    errors.append("Некорректно заполнена дата составления в акте")
+            elif key == "УТВЕНикелинАЮ_дата":
+                stroka = json_for[key][:json_for[key].find("Корреспондирующий счет")]
+                day = stroka[1:stroka[1:].find("\"") + 1]
+                month = stroka[stroka.find(" ") + 1:]
+                age = "".join(month.split(" ")[1:])
+                age = age[:age.find("г.")]
+                month = "".join(month.split(" ")[0])
+                if len(day) != 2 or len(month) < 2 or len(age) != 4:
+                    errors.append("Некорректно заполнена дата в 'УТВЕНикелинАЮ'")
+            elif key == "Первая таблица":
+                table = json_for[key]
+                if table[4] != " ":
+                    errors.append("Неверно заполнено поле структурное подразделение (цех, участок и др.) в таблице")
+                table = table[5:]
+                re_itog = re.search(r'[0-9][0-9][0-9]', table)
+                table = table[re_itog.span()[0] + 3:]
+                re_itog = re.search(r'[0-9][0-9][0-9] ', table)
+                if not re_itog:
+                    errors.append("Отсутствует код операции в таблице")
+                re_itog = re.search(r'[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]', table)
+                if not re_itog:
+                    errors.append("Отсутствует счет. субсчет в таблице")
+                statiya = table[-4:]
+                re_itog = re.search(r'[0-9][0-9][0-9][0-9]', statiya)
+                if not re_itog:
+                    errors.append("Отсутствует статья затрат/носитель затрат в таблице")
+            elif key == "Вторая таблица":
+                table = json_for[key]
+                re_itog = re.search(r'[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]', table)
+                if not re_itog:
+                    errors.append("Некорректный технический счёт 32 \"Затраты\" в таблице")
+                table = table[table.find(" ") + 1:]
+                re_itog = re.search(r'[0-9A-Z][0-9A-Z][0-9A-Z][0-9A-Z][0-9A-Z][0-9A-Z][0-9A-Z][0-9A-Z][0-9A-Z]'
+                                    r'[0-9A-Z][0-9A-Z][0-9A-Z][0-9A-Z][0-9A-Z][0-9A-Z][0-9A-Z][0-9A-Z]', table)
+                if not re_itog:
+                    errors.append("Некорректный производсвенный заказ в таблице")
+                table = table[table.find(" ") + 1:]
+                re_itog = re.search(r'[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]', table)
+                if not re_itog:
+                    errors.append("Некорректные наименование,сорт, размер,марка в таблице")
+                    errors.append("Некорректный номенклатурный номер")
+                else:
+                    table = table[re_itog.span()[1] + 1:]
+                re_itog = re.search(r' [0-9][0-9][0-9] ', table)
+                if not re_itog:
+                    errors.append("Некорректный Заводской номер детали в таблице")
+                re_itog = re.search(r'[0-9][0-9][0-9] ', table)
+                if not re_itog:
+                    errors.append("Некорректный код Единицы измерения")
+                else:
+                    table = table[re_itog.span()[1]:]
+            elif "Председатель0_должность" in json_for.keys():
+                for i in range(0, 10):
+                    if f"Председатель{i}_должность" in json_for.keys():
+                        if json_for[f"Председатель{i}_должность"]:
+                            errors.append(f"Отсутствует у Председателя{i} должность")
+                        if json_for[f"Председатель{i}_подпись"] == {}:
+                            errors.append(f"Отсутствует у Председателя{i} подпись")
+                        if json_for[f"Председатель{i}_рашифровка_подписи"] == {}:
+                            errors.append(f"Отсутствует у Председателя{i} расшифровка подписи")
+            elif "Член_коммисии0_должность" in json_for.keys():
+                for i in range(0, 10):
+                    if f"Член_коммисии{i}_должность" in json_for.keys():
+                        if json_for[f"Член_коммисии{i}_должность"]:
+                            errors.append(f"Отсутствует у Члена комиссии{i} должность")
+                        if json_for[f"Член_коммисии{i}_подпись"] == {}:
+                            errors.append(f"Отсутствует у Члена комиссии{i} подпись")
+                        if json_for[f"Член_коммисии{i}_рашифровка_подписи"] == {}:
+                            errors.append(f"Отсутствует у Члена комиссии{i} расшифровка подписи")
+            elif key == "Мат_отв_лицо_должность":
+                if json_for[key] == {}:
+                    errors.append("Некорректная должность материально ответственного лица")
+            elif key == "Мат_отв_лицо_подпись":
+                if json_for[key] == {}:
+                    errors.append("Некорректная подпись материально ответственного лица")
+            elif key == "Мат_отв_лицо_ФИО":
+                if json_for[key] == {}:
+                    errors.append("Некорректная расшифровка подписи материально ответственного лица")
+            elif key == "Дата":
+                date = json_for[key]
+                try:
+                    time.strptime(date, '%d/%m/%Y')
+                except ValueError:
+                    errors.append("Некорректно заполнена дата подписи материально ответственного лица")
+            else:
+                value = " ".join(json_for[key].split())
+                if value:
+                    json_for[key] = value
+                else:
+                    errors.append(f"Отсутствует значение для '{key}'")
+    errors_json = []
+    if len(errors) == 0:
+        errors = "Корректно заполнен"
+    else:
+        print("Список ошибок: ")
+        for error in set(errors):
+            errors_json += error
+            print("     " + error)
+    json_programm[filename] = errors_json
+    print("-" * 30)
+
 FMU76_json = json.dumps(json_programm)
 with open("FMU76.json", "w") as my_file:
     my_file.write(FMU76_json)
-
-
-
